dc940f6b14dd5914ffa381f8cc343927d05b2d95
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-21 16:26:23 +0100

    Time now returns in JSON

--- src/main/java/com/example/datavisualizationbackend/DemoApplication.java
+++ src/main/java/com/example/datavisualizationbackend/DemoApplication.java
@@ -2,12 +2,7 @@ package com.example.datavisualizationbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.*;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 
 @Controller
 @SpringBootApplication
--- src/main/java/com/example/datavisualizationbackend/TimeController.java
+++ src/main/java/com/example/datavisualizationbackend/TimeController.java
@@ -2,19 +2,20 @@ package com.example.datavisualizationbackend;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @RestController
 public class TimeController {
 
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = GET, produces = "application/json")
     public String getServerTime(){
         Date dateToBeFormatted = new Date();
         SimpleDateFormat datePattern = new SimpleDateFormat("HH:mm a");
         String currentServerTime = datePattern.format(dateToBeFormatted);
-        return "The current time on the server is " + currentServerTime;
+        return "{\"serverTime\":" + currentServerTime + "}";
     }
+
 
 }
