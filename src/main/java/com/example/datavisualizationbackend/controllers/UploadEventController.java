5b2f9352aaa06c938b261ad03fdcde2292424b4a
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-26 12:39:07 +0100

    rabbitMQconfig

--- src/main/java/com/example/datavisualizationbackend/controllers/UploadEventController.java
+++ src/main/java/com/example/datavisualizationbackend/controllers/UploadEventController.java
@@ -1,7 +1,6 @@
 package com.example.datavisualizationbackend.controllers;
 
 import com.example.datavisualizationbackend.conifg.RabbitMQConfig;
-import com.example.datavisualizationbackend.producer.MessageProducer;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
