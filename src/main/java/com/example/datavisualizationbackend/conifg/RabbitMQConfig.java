d40adfbc664f9fb261b35a0e374962a6eb991f2e
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-26 12:30:37 +0100

    rabbitmq config

--- src/main/java/com/example/datavisualizationbackend/conifg/RabbitMQConfig.java
+++ src/main/java/com/example/datavisualizationbackend/conifg/RabbitMQConfig.java
@@ -3,28 +3,88 @@ package com.example.datavisualizationbackend.conifg;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.amqp.core.Queue;
 import org.springframework.context.annotation.Bean;
 
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static java.lang.System.getenv;
+
+
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String ROUTING_KEY = "my.queue.key";
+    protected final String helloWorldQueueName = "hello.world.queue";
 
     @Bean
-    Queue queue() {
-        return new Queue(ROUTING_KEY, true);
+    public ConnectionFactory connectionFactory() {
+        final URI ampqUrl;
+        try {
+            ampqUrl = new URI(getEnvOrThrow("CLOUDAMQP_URL"));
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setUsername(ampqUrl.getUserInfo().split(":")[0]);
+        factory.setPassword(ampqUrl.getUserInfo().split(":")[1]);
+        factory.setHost(ampqUrl.getHost());
+        factory.setPort(ampqUrl.getPort());
+        factory.setVirtualHost(ampqUrl.getPath().substring(1));
+
+        return factory;
     }
 
     @Bean
-    TopicExchange exchange() {
-        return new TopicExchange("my_queue_exchange");
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
     }
 
     @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    public RabbitTemplate rabbitTemplate() {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory());
+        template.setRoutingKey(this.helloWorldQueueName);
+        template.setQueue(this.helloWorldQueueName);
+        return template;
     }
+
+    @Bean
+    public Queue queue() {
+        return new Queue(this.helloWorldQueueName);
+    }
+
+    private static String getEnvOrThrow(String name) {
+        final String env = getenv(name);
+        if (env == null) {
+            throw new IllegalStateException("Environment variable [" + name + "] is not set.");
+        }
+        return env;
+    }
+
+//    public static final String ROUTING_KEY = "my.queue.key";
+//
+//    @Bean
+//    Queue queue() {
+//        return new Queue(ROUTING_KEY, true);
+//    }
+//
+//    @Bean
+//    TopicExchange exchange() {
+//        return new TopicExchange("my_queue_exchange");
+//    }
+//
+//    @Bean
+//    Binding binding(Queue queue, TopicExchange exchange) {
+//        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+//    }
 
 }
--- src/main/java/com/example/datavisualizationbackend/controllers/UploadEventController.java
+++ src/main/java/com/example/datavisualizationbackend/controllers/UploadEventController.java
@@ -1,28 +1,43 @@
 package com.example.datavisualizationbackend.controllers;
 
+import com.example.datavisualizationbackend.conifg.RabbitMQConfig;
 import com.example.datavisualizationbackend.producer.MessageProducer;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.amqp.core.Queue;
 
 @RestController
 @RequestMapping("/upload-event")
 public class UploadEventController {
 
-    @Autowired
-    private MessageProducer messageProducer;
+//    @Autowired
+//    private MessageProducer messageProducer;
 
+    @Autowired private RabbitTemplate amqpTemplate;
+    @Autowired private Queue rabbitQueue;
 
-    @PostMapping
-    public String produce(@RequestBody String message) {
-        System.out.println("Message Received: " + message);
-        messageProducer.sendMessage(message);
-        return "Message Sent to rabbitMQ and message=" + message;
-    }
+//
+//    @PostMapping
+//    public String produce(@RequestBody String message) {
+//        System.out.println("Message Received: " + message);
+//        messageProducer.sendMessage(message);
+//        return "Message Sent to rabbitMQ and message=" + message;
+//    }
 
     @GetMapping
     public String testProduce(){
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(RabbitMQConfig.class);
         String test = "HELLO RABBITMQ";
-        messageProducer.sendMessage(test);
+        AmqpTemplate amqpTemplate = context.getBean(AmqpTemplate.class);
+        amqpTemplate.convertAndSend(test);
+//        messageProducer.sendMessage(test);
+
+        System.out.println("Sent to RabbitMQ: " + test);
         return "Works";
     }
 
