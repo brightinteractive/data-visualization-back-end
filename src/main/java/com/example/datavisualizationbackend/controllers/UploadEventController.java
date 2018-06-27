32715f0223588979fd22c1e39182c70c22205d68
Will Glasse <wg15313@my.bristol.ac.uk>
2018-06-27 14:07:11 +0100

    ready

--- src/main/java/com/example/datavisualizationbackend/controllers/UploadEventController.java
+++ src/main/java/com/example/datavisualizationbackend/controllers/UploadEventController.java
@@ -3,12 +3,9 @@ package com.example.datavisualizationbackend.controllers;
 import com.example.datavisualizationbackend.conifg.RabbitMQConfig;
 import com.example.datavisualizationbackend.models.Event;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.amqp.core.Queue;
 
 @CrossOrigin
 @RestController
